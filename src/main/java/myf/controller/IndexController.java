7e6c0641ef14b270b7868db0b1779052c3c5ea43
wxbiodev01 <abc123>
2020-07-13 15:55:04 +0800

    fix:update settings

--- src/main/java/myf/controller/IndexController.java
+++ src/main/java/myf/controller/IndexController.java
@@ -1,16 +1,16 @@
 package myf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/vue")
 public class IndexController {
 
     @RequestMapping("/index")
-//    @ResponseBody
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name","涨三倍");
         return "index";
     }
 }
