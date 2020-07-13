b798104b1aa5535849325272f26b00032d726bc4
wxbiodev01 <abc123>
2020-07-13 16:13:59 +0800

    fix: delete common-js.js

--- src/main/resources/static/common/common-js.js
+++
@@ -1,7 +0,0 @@
-// put js files below
-document.write('<script src="../js/vue.js" type="text/javascript"></script>');
-document.write('<script src="../js/element-ui@2.12.0.min.js" type="text/javascript"></script>');
-document.write('<script src="../js/jquery-3.1.1.min.js.js" type="text/javascript"></script>');
-
-// put css files below
-document.write('<link href="../css/element-ui@2.12.0.min.css" rel="stylesheet" type="text/css">');
