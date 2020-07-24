a4809472360669a903e03de5e86b3e762a05de76
wxbiodev01 <abc123>
2020-07-24 15:43:47 +0800

    fix: delete common-js.js

--- src/main/java/myf/controller/IndexController.java
+++ src/main/java/myf/controller/IndexController.java
@@ -1,8 +1,14 @@
 package myf.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/vue")
@@ -11,6 +17,36 @@ public class IndexController {
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("name","涨三倍");
-        return "index";
+        return "/index";
+    }
+    @RequestMapping("/user")
+    public String user(Model model){
+//        model.addAttribute("name","涨三倍");
+        return "/menu/user";
+    }
+    @RequestMapping("/dept")
+    public String dept(Model model){
+        model.addAttribute("name","涨三倍");
+        return "/menu/dept";
+    }
+    @RequestMapping("/findMenu")
+    public ResponseEntity findMenu(Model model){
+        List<Map<String,Object>> mapList = new ArrayList<>();
+        for (int i=0;i<2;i++){
+            Map<String,Object> map1 = new HashMap<>();
+            if(i==0){
+                map1.put("system","dept");
+                map1.put("dept","dept"+i);
+                map1.put("url","/vue/dept");
+            }else{
+                map1.put("system","user");
+                map1.put("dept","user"+i);
+                map1.put("url","/vue/user");
+            }
+
+            mapList.add(map1);
+        }
+        model.addAttribute("list",mapList);
+      return   ResponseEntity.ok(mapList);
     }
 }
